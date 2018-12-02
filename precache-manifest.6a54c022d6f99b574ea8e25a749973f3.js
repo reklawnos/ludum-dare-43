59376e5fbd4f06208bcc5a920cfc635463d19a76
Walker Henderson <wjahenderson@gmail.com>
2018-12-01 21:56:43 -0800

    Updates

---
+++ precache-manifest.6a54c022d6f99b574ea8e25a749973f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0620708988f7970fb946",
+    "url": "/ludum-dare-43/static/js/runtime~main.06207089.js"
+  },
+  {
+    "revision": "ed6ee17a8467b4bf8254",
+    "url": "/ludum-dare-43/static/js/main.ed6ee17a.chunk.js"
+  },
+  {
+    "revision": "05f844d0277a4bb365aa",
+    "url": "/ludum-dare-43/static/js/1.05f844d0.chunk.js"
+  },
+  {
+    "revision": "ed6ee17a8467b4bf8254",
+    "url": "/ludum-dare-43/static/css/main.3fe01694.chunk.css"
+  },
+  {
+    "revision": "db9915f5a4f3530f10dfbb84841f8c48",
+    "url": "/ludum-dare-43/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.300ab5ef4f49ca43cf4c4577e4357f29.js"
+  "/ludum-dare-43/precache-manifest.6a54c022d6f99b574ea8e25a749973f3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ludum-dare-43/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
