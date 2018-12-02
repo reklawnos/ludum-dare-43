bd165f775b0612e66b5ed460a292d295aa69ae61
Walker Henderson <wjahenderson@gmail.com>
2018-12-02 00:46:01 -0800

    Add components

---
+++ src/avatars/templates/triangle/triangle_tshirt.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function TriangleTshirt({ baseColor, collarColor }) {
+  return (
+<svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 33.867 33.867"><g transform="translate(0 -263.133)"><ellipse cx="16.992" cy="297.217" rx="13.86" ry="9.841" fill={baseColor}/><path d="M17.236 287.419c-1.931.004-4.918.336-6.67 1.091.908 2.43 4.038 4.777 6.632 4.779 2.543-.002 5.524-2.31 6.467-4.671-2.19-.912-4.353-1.195-6.43-1.2z" fill={collarColor}/></g></svg>); }
