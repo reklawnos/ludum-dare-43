0b252171414ac85d97b9e83c1a7544f21a481569
Walker Henderson <wjahenderson@gmail.com>
2018-12-03 00:00:12 -0800

    Animate money

--- src/MetricMeter.jsx
+++ src/MetricMeter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Spring } from 'react-spring';
 import ImpactMeter from './ImpactMeter';
 
 export default function MetricMeter({
@@ -8,21 +9,27 @@ export default function MetricMeter({
   sizeOfEffect,
   minForFlashing,
 }) {
-  // https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
-  let valueStr = (value * 1000000).toFixed().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   return (
-    <div
-      style={{ display: 'flex' }}
-      className={value < minForFlashing ? 'pulsing-animation' : null }
-    >
-      <div
-       style={{ flexGrow: 1 }}
-      >
-        {prefix}{valueStr}{postfix}
-      </div>
-      <div style={{ flex: '0 0 16px', alignSelf: 'center' }}>
-        <ImpactMeter sizeOfEffect={sizeOfEffect} />
-      </div>
-    </div>
+    <Spring to={{ val: value }}>
+      {({ val }) => {
+        // https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+        let valueStr = (val * 1000000).toFixed().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return (
+          <div
+            style={{ display: 'flex' }}
+            className={value < minForFlashing ? 'pulsing-animation' : null }
+          >
+            <div
+            style={{ flexGrow: 1 }}
+            >
+              {prefix}{valueStr}{postfix}
+            </div>
+            <div style={{ flex: '0 0 16px', alignSelf: 'center' }}>
+              <ImpactMeter sizeOfEffect={sizeOfEffect} />
+            </div>
+          </div>
+        );
+      }}
+    </Spring>
   );
 }
